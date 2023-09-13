9ab27f2567c7d290cca8354c62aa7d5bf7c4f28a
Samuelson23 <3samuelmg@gmail.com>
2023-09-13 18:40:24 +0200

    express server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+const { connect } = require("./db.ts");
+
+dotenv.config();
+const PORT = process.env.PORT;
+const ROUTE = process.env.ROUTE;
+const app = express();
+connect();
+
+app.get("/", (_req, res) => {
+  res.send(`✅Servidor localhost: escuchando ✅`);
+  console.log(`✅Servidor localhost:${PORT} escuchando ✅`);
+});
+app.get("/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("res send, PONG");
+});
+
+//!-------------------------------------------------------
+//?---------------------------PRIMERA RUTA DE NUESTRA API
+//!-------------------------------------------------------
+import diaryRouter from "./routes/diary.routes";
+app.use("/api/v1/routes", diaryRouter);
+
+app.listen(PORT, () => {
+  console.log(`servidor escuchando en el puerto: ${ROUTE}${PORT}`);
+});

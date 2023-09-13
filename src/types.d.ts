cc842324ebb099701f2fc183ef4110cc9ee3da19
Samuelson23 <3samuelmg@gmail.com>
2023-09-13 18:41:04 +0200

    first types

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export type CheckPassword = string | number;
+export type String = string;
+
+export interface UserEntry {
+  name: string;
+  lastName: string;
+  age: number;
+  mobile: number;
+  city: string;
+  email: string;
+}

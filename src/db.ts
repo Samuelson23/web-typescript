651a4d105a8d7bd14c5886df9d29f30cff590bd7
Samuelson23 <3samuelmg@gmail.com>
2023-09-13 18:40:45 +0200

    mongoDB connection

---
+++ src/db.ts
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  try {
+    const dataBase = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const { name, host } = dataBase.connection;
+    console.log(`✅ DDBB connected ✅ In host: ${host} with name: ${name}`);
+  } catch (error) {
+    console.log("❌FAIL connecting DDBB❌");
+    console.log(error);
+  }
+};
+
+module.exports = { connect };
